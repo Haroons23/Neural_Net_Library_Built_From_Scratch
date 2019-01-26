4c74fa52f38e3f7579da72f1db1c0876e3bc2312
Haroon Salimi <hsalimi@uoguelph.ca>
2019-01-26 01:01:15 -0500

    Working on back prop

--- hidden_layers.py
+++ hidden_layers.py
@@ -4,9 +4,10 @@ class HiddenLayer:
 
     # Size of all instance variables are dependant on the previous layer.
     def __init__(self, num_inputs, num_nodes):
-        self.inputs = np.zeros(num_inputs)
+        self.inputs = None #np.zeros(num_inputs)
         self.bias = np.ones(num_nodes)
         self.weights = np.random.random((num_nodes, num_inputs))
+        self.layer_error = np.zeros(num_nodes)
         self.outputs = None #np.zeros(num_inputs)
 
     # Does the summation and puts it through the activation function.
@@ -16,35 +17,76 @@ class HiddenLayer:
         self.outputs = self.weights.dot(self.inputs)
         self.outputs += self.bias
 
+        print("PRE Activation: " + str(self.outputs))
+
         # Putting them through Activation Function (sigmoid).
         for i in range(0, len(self.outputs)):
         	self.outputs[i] = self.sigmoid(self.outputs[i])
 
+        print("POST Activation: " + str(self.outputs))
+
     # Calculating Sigmoid function.
     def sigmoid(self, x, derivative = False):
+
         sigm = 1. / (1. + np.exp(-x))
         if derivative:
             return sigm * (1. - sigm)
         return sigm
+
+
+    # Output Error = sigmoidError(Ci) * (Di - Ci)
+    # Pattern Error = Sum of (Di - Ci) ** 2
+    def calculate_output_error(self, desired):
+        self.layer_error = self.sigmoid(self.outputs, derivative = True) * (desired - self.outputs)
+        print(str(self.layer_error) + " = " + str(self.sigmoid(self.outputs, derivative = True)) + " * (" + str(desired) + " - " + str(self.outputs) + ")")
+        print("LAYER ERROR: " + str(self.layer_error))
+        print("DESIRED: " + str(desired))
+        print("COST: " + str(self.outputs))
+
+        pattern_error = 0
+        for node in range(0, len(self.outputs)):
+            pattern_error += (desired[node] - self.outputs[node]) ** 2
+            print(str(pattern_error) + " += (" + str(desired[node]) + " - " + str(self.outputs[node]) + ")^2")
+
+        return pattern_error
+
+    # Calculates the error of each hidden layer.
+    # ERRORi = sigmoidError(Bi) * Sum of WijEj
+    def calculate_layer_error(self, post_layer_error):
+    
+
+        ## Error with dot product num cols in first matrix != # rows in second matrix ##
+        ## Fix this issue.
+
+        self.layer_error = self.sigmoid(self.outputs, derivative = True) * self.weights.dot(post_layer_error).sum()
+        print("Solving HiddenLayer Error: " + str(self.layer_error) + " = " + str(self.sigmoid(self.outputs, derivative = True)) + " * " + str(self.weights.dot(post_layer_error).sum()))
+        print(">>>>>> " + str(self.layer_error))
 
     # Adding input to the layer before computation.
     def initialize_input(self, inputs):
     	self.inputs = inputs
 
     # Returning output of current layer.
-    def get_output(self)
+    def get_output(self):
         return self.outputs
+
+    # Returning error of current layer.
+    def get_error(self):
+        return self.layer_error
    
     def to_string(self):
         print("inputs: " + str(self.inputs))
         print("bias: " + str(self.bias))
         print("outputs: " + str(self.outputs))
         print("weights: " + str(self.weights))
+        print("error: " + str(self.layer_error))
 
 
 
 
 #i = HiddenLayer(3, 4)
-#i.testing()
+#sigm = i.sigmoid(np.array([1,2,3,4,5,6]))
+#sigm = i.sigmoid(24)
+#print(str(sigm))
 
 
--- neural_network.py
+++ neural_network.py
@@ -4,16 +4,23 @@ import numpy as np
 
 class NeuralNetwork:
 
+    # Training stops when total error is less than max.
+    max_error = 0.05
+
     def __init__(self, num_inputs, num_hidden_layers, num_outputs):
         self.num_inputs  = num_inputs
         self.hidden_layers = []
         self.output_layer = None
         self.create_hidden_layers(num_inputs, num_hidden_layers)
-        self.create_output_layer(num_hidden_layers[-1], num_outputs)
+        self.create_output_layer(num_inputs, num_hidden_layers[-1], num_outputs)
 
 
     # Creates hidden layers. Each array index is one layer. Index value is number of nodes in that hidden layer.
     def create_hidden_layers(self, num_inputs, hidden_layers_info):
+
+        # Creates a perceptron where input goes straight to output node. No hidden layer exists.
+        if hidden_layers_info[0] == 0:
+            return
 
         # Create first layer.
         first_hl = HiddenLayer(num_inputs, hidden_layers_info[0])
@@ -24,34 +31,80 @@ class NeuralNetwork:
             temp_layer = HiddenLayer(hidden_layers_info[layer_num - 1], hidden_layers_info[layer_num])
             self.hidden_layers.append(temp_layer)
 
-    # The number of input going into the output layer is the number of nodes in the last hidden layer.
-    def create_output_layer(self, last_hidden_layer, num_outputs):
-        self.output_layer = HiddenLayer(last_hidden_layer, num_outputs)
+    # The number of input going into the output layer is equal to the number of nodes in the last hidden layer.
+    def create_output_layer(self, num_inputs, last_hidden_layer, num_outputs):
 
-    def forward_prop(self):
+        # Creates a perceptron where input goes straight to output node. No hidden layer exists.
+        if last_hidden_layer == 0:
+            self.output_layer = HiddenLayer(num_inputs, num_outputs)
 
-        test_data = np.array([10, 20])
+        # When a hidden layer exists.
+        else:
+            self.output_layer = HiddenLayer(last_hidden_layer, num_outputs)
 
-        self.hidden_layers[0].initialize_input(test_data)
-        self.hidden_layers[0].calculate_output()
-        self.hidden_layers[0].to_string()
+        self.hidden_layers.append(self.output_layer)
+
+    def train(self, data, learning_rate, momentum):
+
+        cumulative_error = 1000000
+        epoch = 0
+        data = np.array([[.10, .20], [.30, .40]])
+        #desired = np.array([[1, 1], [1, 1]])
+        desired = np.array([[1], [1]])
+
+        # Train until you run out of data or error is below limit.
+        while epoch < 1 and cumulative_error > self.max_error:
+            
+            for i in range(0, len(data)):
+                self.forward_prop(data[i])
+                cumulative_error += self.backward_prop(learning_rate, momentum, desired[i])
 
 
+            epoch += 1
 
 
+    # Forward Propagation.
+    def forward_prop(self, data):
 
+        for hidden_layer in self.hidden_layers:
+            print("------HIDDEN LAYER------")
+
+            # Initializing input data for layer to be evaluated.
+            hidden_layer.initialize_input(data)
+            hidden_layer.calculate_output()
+
+            # Collecting evaluated output because it is the input to the next layer.
+            data = hidden_layer.get_output()
+            hidden_layer.to_string()
+
+    # Backward Propagation.
+    def backward_prop(self, learning_rate, momentum, desired):
+    
+        # Calculating output error.
+        pattern_error = self.output_layer.calculate_output_error(desired)
+        post_layer_error = self.output_layer.get_error()
+
+        # Calculating hidden layer errors starting with the last one.
+        for layer in range(len(self.hidden_layers) - 1, -1, -1):
+            self.hidden_layers[layer].calculate_layer_error(post_layer_error)
+            print("Bitch!")
+            #post_layer_error = self.hidden_layers[layer].get_error()
+        
+        return pattern_error
+
+
+        
 
     def to_string(self):
         for i in self.hidden_layers:
             i.to_string()
- 
         print("\n")
         self.output_layer.to_string()
 
 
-n = NeuralNetwork(2, [3, 2], 1)
-n.forward_prop()
-#n.to_string()
+n = NeuralNetwork(2, [1], 1)
+#n.forward_prop()
+n.train(1, .05, .10)
 
 
         
